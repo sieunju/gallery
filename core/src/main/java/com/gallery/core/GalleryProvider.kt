8fa6984f6790043f29df55c109a5d42f715899bb
sieunju <j.sieun73@gmail.com>
2022-09-13 21:45:38 +0900

    feat: core, ui, edit 모듈 환경 구축 - 각 모듈별 작업할수 있는 환경 구축

---
+++ core/src/main/java/com/gallery/core/GalleryProvider.kt
@@ -0,0 +1,9 @@
+package com.gallery.core
+
+/**
+ * Description : 갤러리 사진 데이터들 가져오는 Interface
+ *
+ * Created by juhongmin on 2022/09/13
+ */
+interface GalleryProvider {
+}
---
+++ core/src/main/java/com/gallery/core/impl/GalleryProviderImpl.kt
@@ -0,0 +1,15 @@
+package com.gallery.core.impl
+
+import android.content.Context
+import com.gallery.core.GalleryProvider
+import javax.inject.Inject
+
+/**
+ * Description :
+ *
+ * Created by juhongmin on 2022/09/13
+ */
+class GalleryProviderImpl @Inject constructor(
+    val context: Context
+) : GalleryProvider {
+}
