f236e28264b9cd1942f5b523ba37ce032e5bc5a9
sieunju <j.sieun73@gmail.com>
2022-09-17 09:56:49 +0900

    feat: 갤러리 이미지 편집하는 RootLayout 추가

---
+++ edit/src/main/java/com/gallery/edit/GalleryEditView.kt
@@ -0,0 +1,21 @@
+package com.gallery.edit
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+/**
+ * Description : Gallery Edit Root Layout
+ *
+ * Created by juhongmin on 2022/09/17
+ */
+class GalleryEditView @JvmOverloads constructor(
+    ctx: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(ctx, attrs, defStyleAttr) {
+
+    init {
+        
+    }
+}
