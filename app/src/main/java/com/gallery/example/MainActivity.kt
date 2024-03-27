f9a4f9ce5f814b13ac82a4bc39c62a870af15896
Sieun Ju <33802191+sieunju@users.noreply.github.com>
2024-03-27 09:12:53 +0900

    chore: 뒤로가기 네비게이션 처리 추가

--- app/src/main/java/com/gallery/example/MainActivity.kt
+++ app/src/main/java/com/gallery/example/MainActivity.kt
@@ -1,5 +1,19 @@
 package com.gallery.example
 
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {}
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val onBackPressCallback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            finishAffinity()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        onBackPressedDispatcher.addCallback(this, onBackPressCallback)
+    }
+}
